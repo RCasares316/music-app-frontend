660fb69b29fc48ec54a54dbd5cf453d13890332d
JEStudyhall <jaieelizabeth@gmail.com>
2026-02-11 11:14:50 -0500

    List-create-update

--- src/components/PlayListList/PlayListList.jsx
+++ src/components/PlayListList/PlayListList.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { getPlaylists } from "../../services/playlist.js";
 
 const PlayListList = () => {
-  return (
-    <div>PlayListList</div>
-  )
-}
+  const [playlists, setPlaylists] = useState([]);
+  useEffect(() => {
+    const loadPlaylists = async () => {
+      try {
+        const data = await getPlaylists();
+        setPlaylists(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    loadPlaylists();
+  }, []);
 
-export default PlayListList
+  return (
+    <main>
+      {playlists.map((playlist) => (
+        <Link key={playlist._id} to={`/playlists/${playlist._id}`}>
+          <article>
+            <h2>{playlist.name}</h2>
+            {playlist.img && (
+              <img src={playlist.img} alt={playlist.name} />
+            )}
+
+          </article>
+        </Link>
+      ))}
+    </main>
+  );
+};
+
+export default PlayListList;
