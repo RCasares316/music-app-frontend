9f071cc0fad32f728f0f77009bcd2c6b053e6bd4
RCasares316 <richiecasares@gmail.com>
2026-02-11 10:06:42 -0500

    updates made

--- src/services/tracks.js
+++ src/services/tracks.js
@@ -1,1 +1,10 @@
-import api from "./apiConfig.js"
+import api from "./apiConfig.js";
+
+export const getTracks = async (req, res) => {
+  try {
+    const { data } = await api.get("/tracks");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
