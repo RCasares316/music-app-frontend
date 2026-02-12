1c0e1b390f942b4c8483f1d3f15b5295463b0db6
RCasares316 <richiecasares@gmail.com>
2026-02-12 11:21:40 -0500

    updating add track functionality

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { getPlaylist } from "../services/playlist.js";
 
 const UserContext = createContext();
 
@@ -12,9 +13,18 @@ const getUserFromToken = () => {
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState(getUserFromToken());
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      const playlistsData = await getPlaylist();
+      setPlaylists(playlistsData);
+    };
+    fetchPlaylists();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, setUser, playlists }}>
       {children}
     </UserContext.Provider>
   );
